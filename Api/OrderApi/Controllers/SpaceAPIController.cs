7a50a9774313522e8bcb9a3cf8af2946c16badb5
Berend Wouters <berend.wouters@gmail.com>
2019-03-26 23:06:51 +0100

    get current spaceApi state

--- Api/OrderApi/Controllers/SpaceAPIController.cs
+++ Api/OrderApi/Controllers/SpaceAPIController.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Brixel.SpaceAPI.Core.Models;
+using Brixel.SpaceAPI.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrderApi.Controllers
 {
     public class SpaceAPIController : Controller
     {
+        private readonly ISpaceAPIService _apiService;
         private HttpClient _client;
 
-        public SpaceAPIController()
+        public SpaceAPIController(ISpaceAPIService apiService)
         {
-            _client = new HttpClient();
-            _client.BaseAddress = new Uri("https://spaceapi.azurewebsites.net");
-            
+            _apiService = apiService;
         }
-        public void GetCurrentSpaceState()
+
+        [HttpGet("spaceapi/status")]
+        public async Task<SpaceAPIRoot> GetCurrentSpaceState()
         {
-            
+            var state =  await _apiService.GetSpaceApiAsync();
+            return state;
         }
     }
 }
